d80cdde8f720e754861619560729b0a1a4139fbc
yeoni <happyksy0501@gmail.com>
2026-07-02 20:44:48 +0900

    chore: lora_config 주석 추가

--- backend/src/llm_models/model_config.py
+++ backend/src/llm_models/model_config.py
@@ -1,1 +1,21 @@
-#하이퍼파라미터
+from peft import LoraConfig
+
+lora_config = LoraConfig(
+
+    r=16,  # r=8 or 16이 적정 64는 vram너무 잡아먹음
+
+    lora_alpha=32, #ΔW × alpha/r ->lora효과 2배적용 (16*2)
+
+    lora_dropout=0.05, #과적합 방지
+
+    bias="none",  #메모리 최적화.. 덜 쓰려고
+
+    task_type="CAUSAL_LM",
+
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj"
+    ]  #gate,up,down_proj은 적용 x -> vram과다 사용방지
+)
